43433fb2ac655450edff2d6d50fd1734d7a534d6
Myang09 <xghujjk@gmail.com>
2026-05-07 01:15:42 +0900

    수정

--- fronted/src/pages/LoginPage.vue
+++ fronted/src/pages/LoginPage.vue
@@ -51,7 +51,7 @@ const submit = async () => {
             <input
               v-model="form.email"
               type="email"
-              placeholder="이메일 입력 ㄱ!!!!!!!"
+              placeholder="이메일 입력 ㄱㄱ!!!!!!!!!"
               class="form-input auth-plain-input !min-h-[52px]"
               :class="errors.email ? 'border-danger focus:ring-0' : ''"
             />
